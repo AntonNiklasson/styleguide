86fe35fefaa4423834a8c84b6b4a018221c84b59
Atanas Nikolov <atanas.nikolov@acando.com>
2017-12-07 17:09:20 +0100

    Create header.

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="header">
+        <div className="header__container">
+          <div className="header__container-logo" />
+        </div>
+      </div>
+    );
+  }
+}
