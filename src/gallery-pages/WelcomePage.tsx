6b0e9fc81f2e6fcccc26ca70a0e490159e7b3103
Atanas Nikolov <atanas.nikolov@acando.com>
2017-12-07 17:13:03 +0100

    Wlecome page in gallery container.

---
+++ src/gallery-pages/WelcomePage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default class WelcomePage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Välkommen till komponent biblioteket!</h3>
+      </div>
+    );
+  }
+}
