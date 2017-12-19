c7bb9d8420576efe6b373e14d343d5df3ec159dc
Atanas Nikolov <atanas.nikolov@acando.com>
2017-12-19 18:04:07 +0100

    Create textpuffar component.

---
+++ src/gallery-pages/TextArticlePage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import TextArticle from '../components/text-article/TextArticle';
+
+export default class TextArticlePage extends React.Component {
+  render() {
+    return (
+      <div className="full-height">
+        <div className="spacer-mb-large spacer-mt-large text-center">
+          <h1 className="h1">Textpuffar</h1>
+        </div>
+        <TextArticle />
+      </div>
+    );
+  }
+}
