b441b680b503f9d5d2972e4a7fc61bd1fe6d2273
Atanas Nikolov <atanas.nikolov@acando.com>
2018-01-26 12:36:27 +0100

    Fix navigation-menu container.

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -11,41 +11,51 @@ export default class MobileMenu extends React.Component<MobileMenuParameters> {
     return (
       <div className={`${menuState}`}>
         <div className="mobile-menu mobile-menu__header">
-          <div className="mobile-menu__container">
-            <h1 className="h1 mobile-menu__header-title">Meny</h1>
-            <a href="#" className="icon-close text-small" onClick={this.props.toggleMenu}>
-              Stäng
-            </a>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div className="mobile-menu__header-container">
+                  <h1 className="h1 mobile-menu__header-title">Meny</h1>
+                  <a href="#" className="icon-close text-small" onClick={this.props.toggleMenu}>
+                    Stäng
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="mobile-menu mobile-menu__links">
-          <div className="mobile-menu__container">
-            <ul className="mobile-menu__list">
-              <li className="mobile-menu__list-item">
-                <a href="#" className="mobile-menu__list-item--link text-medium">
-                  Undervisning
-                  <i className="icon-arrow"/>
-                </a>
-              </li>
-              <li className="mobile-menu__list-item">
-                <a href="#" className="mobile-menu__list-item--link text-medium">
-                  Skolutveckling
-                  <i className="icon-arrow"/>
-                </a>
-              </li>
-              <li className="mobile-menu__list-item">
-                <a href="#" className="mobile-menu__list-item--link text-medium">
-                  Lagar och regler
-                  <i className="icon-arrow"/>
-                </a>
-              </li>
-              <li className="mobile-menu__list-item">
-                <a href="#" className="mobile-menu__list-item--link text-medium">
-                  Innehåll A-Ö
-                  <i className="icon-arrow"/>
-                </a>
-              </li>
-            </ul>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <ul className="mobile-menu__list">
+                  <li className="mobile-menu__list-item">
+                    <a href="#" className="mobile-menu__list-item--link text-medium">
+                      Undervisning
+                      <i className="icon-arrow" />
+                    </a>
+                  </li>
+                  <li className="mobile-menu__list-item">
+                    <a href="#" className="mobile-menu__list-item--link text-medium">
+                      Skolutveckling
+                      <i className="icon-arrow" />
+                    </a>
+                  </li>
+                  <li className="mobile-menu__list-item">
+                    <a href="#" className="mobile-menu__list-item--link text-medium">
+                      Lagar och regler
+                      <i className="icon-arrow" />
+                    </a>
+                  </li>
+                  <li className="mobile-menu__list-item">
+                    <a href="#" className="mobile-menu__list-item--link text-medium">
+                      Innehåll A-Ö
+                      <i className="icon-arrow" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -26,56 +26,68 @@ export default class Navigation extends React.Component<object, NavigationState>
         <MobileMenu menuState={this.state.isOpen} toggleMenu={this.toggleMenu} />
         <div className={`${mobileMenuState}`}>
           <div className="navigation navigation__header">
-            <div className="nav-container nav-container--header">
-              <div className="navigation__header-logo"/>
-              <ul className="navigation__list">
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--speaker text-small">
-                    Lyssna
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--language text-small">
-                    Languages
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--search text-small">
-                    Sök
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--mobilemenu text-small" onClick={this.toggleMenu}>
-                    Meny
-                </a>
-                </li>
-              </ul>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <div className="navigation__header-container">
+                    <div className="navigation__header-logo" />
+                    <ul className="navigation__list">
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--speaker text-small">
+                          Lyssna
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--language text-small">
+                          Languages
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--search text-small">
+                          Sök
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--mobilemenu text-small" onClick={this.toggleMenu}>
+                          Meny
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="navigation navigation__links">
-            <div className="nav-container nav-container--links">
-              <ul className="navigation__list navigation__list--absolute">
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--link text-medium">
-                    Undervisning
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--link text-medium">
-                    Skolutveckling
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--link text-medium">
-                    Lagar och regler
-                </a>
-                </li>
-                <li className="navigation__list-item">
-                  <a href="#" className="navigation__list-item--link text-standard">
-                    Innehåll A-Ö
-                </a>
-                </li>
-              </ul>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <div className="navigation__links-container">
+                    <ul className="navigation__list navigation__list--absolute">
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--link text-medium">
+                          Undervisning
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--link text-medium">
+                          Skolutveckling
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--link text-medium">
+                          Lagar och regler
+                        </a>
+                      </li>
+                      <li className="navigation__list-item">
+                        <a href="#" className="navigation__list-item--link text-standard">
+                          Innehåll A-Ö
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
