58f319f9582e36ee35bdeb2829e92aafe5af8e23
Martin Jansson <martify@googlemail.com>
2018-02-15 14:54:42 +0100

    Updates

--- _styleguide/js/well.js
+++ _styleguide/js/well.js
@@ -1,3 +1,11 @@
+  console.log('well startswith');
+  if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(search, pos) {
+      return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+    };
+  }
+
+
 (function (){
 
   function global (str) {
