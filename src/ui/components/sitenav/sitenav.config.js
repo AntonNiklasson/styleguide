0f68ffd8dfd4d82acf6d5f88d704be33b5d555c2
Anton Niklasson <niklasson.anton@gmail.com>
2018-10-24 16:58:29 +0200

    add sitenav and siteheader

---
+++ src/ui/components/sitenav/sitenav.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'Huvudmeny',
+	handle: 'sitenav',
+	context: {
+		links: [
+			{ label: 'Lorem Ipsum', url: "#" },
+			{ label: 'Lorem Ipsum', url: "#" },
+			{ label: 'Lorem Ipsum', url: "#" },
+			{ label: 'Lorem Ipsum', url: "#", active: true },
+			{ label: 'Lorem Ipsum', url: "#" },
+		]
+	}
+}
