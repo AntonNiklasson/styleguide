f690d425106320dd61f2ac2090424ec043a2ae98
Anton Niklasson <niklasson.anton@gmail.com>
2018-10-24 15:58:49 +0200

    add content navigation component

---
+++ src/ui/containers/content-navigation/content-navigation.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'Innehållsnavigering',
+	context: {
+		title: 'Områdestitel',
+		description: 'Kontextuell fördjupningstext.',
+		links: [
+			{ label: 'Områdeslänk', url: '#1' },
+			{ label: "Phasellus mattis diam convallis lectus ultricies", url: "#" },
+			{ label: 'Phasellus convallis lectus ultricies', url: '#3' },
+			{ label: 'Områdeslänk', url: '#1' },
+			{ label: "Phasellus mattis diam convallis lectus ultricies", url: "#" },
+			{ label: 'Phasellus convallis lectus ultricies', url: '#3' }
+		]
+	}
+}
