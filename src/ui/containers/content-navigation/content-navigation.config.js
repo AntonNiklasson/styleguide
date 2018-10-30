06e410f963650fd2df272266cdddc4a6ab91a44e
Alexander Darland <alexander.darland@creuna.se>
2018-10-30 17:17:44 +0100

    Updated model and added 2 columns. Added variant with text.

--- src/ui/containers/content-navigation/content-navigation.config.js
+++ src/ui/containers/content-navigation/content-navigation.config.js
@@ -3,13 +3,32 @@ module.exports = {
 	context: {
 		title: 'Områdestitel',
 		description: 'Kontextuell fördjupningstext.',
-		links: [
-			{ label: 'Områdeslänk', url: '#1' },
+		columnOne: [
+			{ label: 'Områdeslänk', url: '#' },
 			{ label: "Phasellus mattis diam convallis lectus ultricies", url: "#" },
-			{ label: 'Phasellus convallis lectus ultricies', url: '#3' },
-			{ label: 'Områdeslänk', url: '#1' },
-			{ label: "Phasellus mattis diam convallis lectus ultricies", url: "#" },
-			{ label: 'Phasellus convallis lectus ultricies', url: '#3' }
-		]
-	}
+			{ label: 'Phasellus convallis lectus ultricies', url: '#' }
+		],
+    columnTwo: [
+      { label: 'Områdeslänk', url: '#1' },
+      { label: "Phasellus mattis diam convallis lectus ultricies", url: "#" },
+      { label: 'Phasellus convallis lectus ultricies', url: '#' }
+    ]
+	},
+  variants: [
+    {
+      name: 'Med länktext',
+      context: {
+        columnOne: [
+          { label: 'Områdeslänk', url: '#', description: 'Sed ut perspiciatis unde omnis iste natus.' },
+          { label: "Phasellus mattis diam convallis lectus ultricies", url: "#", description: 'Doloremque laudantium, totam rem aperiam ipsa quae ab unde omnis.' },
+          { label: 'Phasellus convallis lectus ultricies', url: '#', description: 'Totam rem aperiam, eaque ipsa quae ab illo inventore.' }
+        ],
+        columnTwo: [
+          { label: 'Områdeslänk', url: '#1', description: 'Sed ut perspiciatis unde omnis iste natus sed ut perspiciatis unde omnis iste natus.' },
+          { label: "Phasellus mattis diam convallis lectus ultricies", url: "#", description: 'Doloremque totam rem aperiam.' },
+          { label: 'Phasellus convallis lectus ultricies', url: '#', description: 'Totam rem aperiam, eaque ipsa quae ab illo inventore.' }
+        ]
+      }
+    }
+  ]
 }
