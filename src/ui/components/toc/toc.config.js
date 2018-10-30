f0669692a99264c7fb4257831a9b8040fb1dd266
Alexander Darland <alexander.darland@creuna.se>
2018-10-30 15:57:48 +0100

    Simplified model to match todays' styleguide. Added missing styles for links.

--- src/ui/components/toc/toc.config.js
+++ src/ui/components/toc/toc.config.js
@@ -1,44 +1,32 @@
 module.exports = {
   name: "Innehållsförteckning",
   context: {
-    title: "Innehållsförteckning",
+    title: "Rubrik",
     pages: [
       {
-        title: "Lorem Ipsum",
-        url: "#",
-        pages: [{ title: "Dolor Sit Amet", url: "#" }]
-      },
-      { title: "Lorem Ipsum", url: "#", active: true },
-      {
-        title: "Lorem",
+        title: "Innehållsrubrik nivå 1",
         url: "#",
         pages: [
-          { title: "Ipsum Dolor", url: "#" },
-          { title: "Sit Amet", url: "#" },
           {
-            title: "Ipsum Lorem",
-            url: "#",
-            pages: [{ title: "Amet Sit", url: "#" }]
+            title: "Innehållsrubrik nivå 2",
+            url: "#"
           },
-          { title: "Ipsum Sit", url: "#" },
-          { title: "Dolor Lorem", url: "#" }
+          {
+            title: "Innehållsrubrik nivå 2",
+            url: "#"
+          }
         ]
       },
-      { title: "Sit Amet", url: "#" },
-      { title: "Lorem Dolor", url: "#" }
-    ]
-  },
-  variants: [
-    {
-      name: "Utan Rubrik",
-      context: {
-        title: null,
-        pages: [
-          { title: "Den här", url: "#" },
-          { title: "innehålssförteckningen", url: "#" },
-          { title: "har ingen rubrik", url: "#" }
-        ]
+      {
+        title: "Innehållsrubrik nivå 1",
+        url: "#",
+        pages: null
+      },
+      {
+        title: "Innehållsrubrik nivå 1",
+        url: "#",
+        pages: null
       }
-    }
-  ]
+    ]
+  }
 };
