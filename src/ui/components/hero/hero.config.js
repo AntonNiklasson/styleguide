e3ac3c5c8c4930a744fd1821220c1367d1477a82
Anton Niklasson <niklasson.anton@gmail.com>
2018-11-01 10:56:00 +0100

    add subpage page

--- src/ui/components/hero/hero.config.js
+++ src/ui/components/hero/hero.config.js
@@ -3,5 +3,14 @@ module.exports = {
     background: '/tmp/img/bildstartsida.png',
     title: 'En lång rubrik som sträcker sig över flera rader',
     url: '#'
-  }
+  },
+  variants: [
+    {
+      name: 'Without title',
+      context: {
+        title: null,
+        background: '/tmp/img/bildstartsida.png',
+      }
+    }
+  ]
 };
