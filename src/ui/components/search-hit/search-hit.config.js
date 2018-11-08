85afbfa299333c56825d4424589f288db2ac5a8d
Anton Niklasson <niklasson.anton@gmail.com>
2018-11-08 09:59:31 +0100

    add search hit component

---
+++ src/ui/components/search-hit/search-hit.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Sökträff',
+  handle: 'search-hit',
+  context: {
+    title: 'Lorem ipsum dolir sit amet',
+    url: '#',
+    body: 'Dolore occaecat eu labore sit pariatur do laboris commodo laboris exercitation dolore do sit do. Tempor deserunt ea occaecat occaecat est laboris. Duis dolore laboris laboris adipisicing. Ullamco nostrud ea sit aute quis veniam cupidatat sit in est deserunt mollit Lorem elit. Adipisicing dolore ex deserunt fugiat elit ut duis non consequat ut.',
+    path: [
+      { url: '#', label: 'Lorem' },
+      { url: '#', label: 'Lorem' },
+      { url: '#', label: 'Lorem' },
+      { url: null, label: 'Lorem' },
+    ]
+  }
+}
