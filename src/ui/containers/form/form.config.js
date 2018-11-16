ea419206c900cd3093a501150611238e73b98e18
Anton Niklasson <niklasson.anton@gmail.com>
2018-11-16 12:10:26 +0100

    update the search page with checkboxes in a sidebar

--- src/ui/containers/form/form.config.js
+++ src/ui/containers/form/form.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   name: 'Formulär',
-  handle: 'form'
+  handle: 'form',
+  variants: [
+    {
+      name: 'checkboxes',
+      context: {
+        checkboxes: [
+          { label: 'Kryssboxval 1', id: 'option-1' },
+          { label: 'Kryssboxval 2', id: 'option-2' },
+          { label: 'Lorem', id: 'lorem' },
+          { label: 'Ipsum', id: 'ipsum' },
+          { label: 'Dolor sit amet', id: 'dolor-sit-amet' }
+        ]
+      }
+    }
+  ]
 }
--- src/ui/pages/search-page/search-page.config.js
+++ src/ui/pages/search-page/search-page.config.js
@@ -4,6 +4,45 @@ module.exports = {
   context: {
     searchModel: {
       placeholder: 'Sök på hela skolverket.se'
-    }
+    },
+    searchModel: {
+      title: 'anton niklasson',
+      placeholder: 'Sök på sidor och innehåll i detta filter'
+    },
+    checkboxGroups: [
+      {
+        title: 'Skolform eller verksamhet',
+        checkboxes: [
+          { label:'Annan pedagogisk verksamhet', id: 'lorem' },
+          { label:'Dansarutbildning', id: 'lorem' },
+          { label:'Fritidshem', id: 'lorem' },
+          { label:'Förskola', id: 'lorem' },
+          { label:'Förskoleklass', id: 'lorem' },
+          { label:'Grundskoleutbildning', id: 'lorem' },
+          { label:'Gymnasieutbildning', id: 'lorem' },
+          { label:'Utbildning utomlands', id: 'lorem' },
+          { label:'Vuxenutbildning', id: 'lorem' },
+        ]
+      },
+      {
+        title: 'Vem kan söka?',
+        checkboxes: [
+          { label:'Fristående huvudman', id: 'lorem' },
+          { label:'Kommunal huvudman', id: 'lorem' },
+          { label:'Organisation Statlig huvudman', id: 'lorem' },
+        ]
+      },
+      {
+        title: 'Fokusområde',
+        checkboxes: [
+          { label:'Arbetsplatsförlagt lärande', id: 'lorem' },
+          { label:'Bedömning och betyg', id: 'lorem' },
+          { label:'Didaktik', id: 'lorem' },
+          { label:'Digitalisering', id: 'lorem' },
+          { label:'Elevhälsa', id: 'lorem' },
+          { label:'Entreprenörskap', id: 'lorem' },
+        ]
+      }
+    ]
   }
 }
