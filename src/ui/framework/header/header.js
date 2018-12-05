9882c935dbf17e63c4204ca90418a922c708da1a
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-05 16:06:38 +0100

    Prefix all classes with `skv-` & delete old scss files (#45)
    * prefix all classes among components
    
    * prefixing container classes
    
    * class prefix for all pages and framework
    
    * remove all that legacy scss
    
    * prefix reveal classes with skv-, fix the reveals timing function
    
    * prefix form classes
    
    * Fixed faulty commit

--- src/ui/framework/header/header.js
+++ src/ui/framework/header/header.js
@@ -1,9 +1,9 @@
-var items = document.querySelectorAll('.header__list-item-mobilemenu, .icon-close');
+var items = document.querySelectorAll('.skv-header__list-item-mobilemenu, .icon-close');
 
 for (var i = 0; i < items.length; i++) {
 	items[i].addEventListener('click', function(e) {
 		e.preventDefault();
-		document.querySelector('.mobile-menu').classList.toggle('mobile-menu--open');
-		document.querySelector('.header').classList.toggle('header--close');
+		document.querySelector('.skv-mobile-menu').classList.toggle('skv-mobile-menu--open');
+		document.querySelector('.skv-header').classList.toggle('skv-header--close');
 	});
 }
