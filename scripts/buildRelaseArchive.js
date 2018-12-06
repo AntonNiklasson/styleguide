94bbb4d435410e3c085c69ce371181a5aebe38df
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-06 11:41:40 +0100

    add support for mapping folders in the release archive

--- scripts/buildRelaseArchive.js
+++ scripts/buildRelaseArchive.js
@@ -7,15 +7,46 @@ const pkg = require('../package.json')
 
 const archiveName = `skolverket-styleguide-${pkg.version}`;
 const files = {
-  './dist/js/main.min.js': 'main.min.js',
-  './dist/css/main.min.css': 'main.min.css',
+  'dist/js/main.min.js': 'main.min.js',
+  'dist/css/main.min.css': 'main.min.css',
+  'dist/assets/': 'assets/',
   'README.md': 'README.md'
 }
 
-Object.entries(files).forEach(([source, destination]) => {
-  const sourceData = fs.readFileSync(path.resolve(__dirname, '..', source))
+const isDirectory = name => {
+  return fs.lstatSync(name).isDirectory()
+}
 
-  zip.file(path.join(archiveName, destination), sourceData)
+const getSubtree = (folder, alias) => {
+  let tree = {};
+  const files = fs.readdirSync(folder)
+  
+  files.forEach(file => {
+    const filepath = path.join(folder, file);
+
+    if (isDirectory(filepath)) {
+      const subtree = getSubtree(filepath, filepath.replace(folder, alias))
+      tree = { ...tree, ...subtree };
+    } else {
+      tree[filepath.replace(folder, alias)] = fs.readFileSync(filepath)
+    }
+  })    
+
+  return tree;
+}
+
+Object.entries(files).forEach(([source, destination]) => {
+
+  if (isDirectory(source)) {
+    const subtree = getSubtree(source, destination);
+
+    Object.entries(subtree).forEach(([filepath, data]) => {
+      zip.file(path.join(archiveName, filepath), data);
+    });
+  } else {
+    const sourceData = fs.readFileSync(path.resolve(__dirname, '..', source))
+    zip.file(path.join(archiveName, destination), sourceData)
+  }
 });
 
 fs.writeFileSync(
