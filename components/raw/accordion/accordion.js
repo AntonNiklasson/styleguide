4af8fa1d352ed829cc94244d9b6525a8f18bfbbe
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-06 12:57:44 +0100

    Updates

--- components/raw/accordion/accordion.js
+++
@@ -1,6 +0,0 @@
-var items = document.querySelectorAll('.skv-accordion__item-link');
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener('click', function() {
-    this.closest(".skv-accordion__item").classList.toggle('skv-accordion__item--open');
-  });
-}
